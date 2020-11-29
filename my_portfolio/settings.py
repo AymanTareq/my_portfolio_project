9f85e2891e199ac72fd7ae624c5b58d927dcc45c
AymanTareq.97 <tareq.ctec02@gmail.com>
2020-11-29 13:57:41 +0600

    added static_root in settings

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR/ 'static'
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
